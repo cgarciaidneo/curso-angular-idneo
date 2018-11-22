bef5c285e63ba9ec6bef4aa843ca8d08d62826d9
Cristian García <cristian.garcia@idneo.com>
2018-11-22 19:42:55 +0100

    (Progress) Creación de la estructura de la sección "keep": creado el menu superior, en progreso la navegación lateral.

--- src/app/section/keep/keep.module.ts
+++ src/app/section/keep/keep.module.ts
@@ -3,11 +3,17 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { keepRoutes } from './keep.router';
+import { TopBarComponent } from './partial/navigation/top-bar/top-bar.component';
+import { SideNavComponent } from './partial/navigation/side-nav/side-nav.component';
+import { SearchBarComponent } from './partial/navigation/top-bar/search-bar/search-bar.component';
 
 
 @NgModule({
   declarations: [
-    KeepComponent
+    KeepComponent,
+    TopBarComponent,
+    SideNavComponent,
+    SearchBarComponent
   ],
   imports: [
     RouterModule.forChild(keepRoutes),
